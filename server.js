acec23e6948075be6b90204cb4b2173957900580
jattorri <jattorri@gmail.com>
2021-02-22 17:08:25 -0300

    sexto

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static('./AngularWorkspace/rrezi'));
+app.use(express.static('./dist/rrezi'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'AngularWorkspace/rrezi'}),
+    res.sendFile('index.html', {root: 'dist/rrezi'}),
 );
 
 
 app.listen(process.env.PORT || 8080);
+
+console.log(`Running on port ${process.env.PORT || 8080}`)
